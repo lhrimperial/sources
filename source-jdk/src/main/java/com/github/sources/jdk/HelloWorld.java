d0cca4c0fa1c29ee6851536e43bc795c05156851
lhrimperial <lhr9563216@163.com>
2018-09-12 08:13:09 +0800

    modify

---
+++ source-jdk/src/main/java/com/github/sources/jdk/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.github.sources.jdk;
+
+/**
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        Integer i = 1;//通过java/lang/Integer.valueOf:(I)包装
+        String a = "abc";
+        String b = "def";
+        System.out.println(i);
+        System.out.println(a+b); //通过java/lang/StringBuilder.append:(Ljava/lang/String;)拼接
+    }
+}
