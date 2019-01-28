857c21949c37d974a29af79b537e60a1b68aa0a0
lhr <lhr9563215@163.com>
2019-01-29 00:31:57 +0800

    commit

--- source-docker/source-docker-sboot/src/main/java/com/github/sources/docker/sboot/controller/IndexController.java
+++ source-docker/source-docker-sboot/src/main/java/com/github/sources/docker/sboot/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.github.sources.docker.sboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/docker")
 public class IndexController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @RequestMapping("/getIncrement")
+    public String getIncrement() {
+        Long increment = redisTemplate.opsForValue().increment(1, 1);
+        return "This increment：" + increment;
+    }
 
     @GetMapping("/getValue")
     public String getValue() {
