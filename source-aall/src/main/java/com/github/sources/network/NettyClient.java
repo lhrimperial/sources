f4ad64427a7c03022e5c31a784dc7d4d964c0600
hairen.lhr <hairen.lhr@alibaba-inc.com>
2020-09-27 10:17:32 +0800

    merge

--- source-aall/src/main/java/com/github/sources/network/NettyClient.java
+++ source-aall/src/main/java/com/github/sources/network/NettyClient.java
@@ -1,65 +1,12 @@
-<<<<<<< HEAD
 package com.github.sources.network;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.*;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
-
-/**
- *
- **/
-public class NettyClient {
-
-    public static void main(String[] args) {
-        new NettyClient().start("127.0.0.1", 8080);
-    }
-
-    public void start(String host, int port) {
-        EventLoopGroup group = new NioEventLoopGroup();
-
-        try {
-            Bootstrap client = new Bootstrap().group(group).channel(NioSocketChannel.class)
-                    .option(ChannelOption.TCP_NODELAY, true)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline()
-                                    .addLast("decode", new StringDecoder())
-                                    .addLast("encode", new StringEncoder())
-                                    .addLast(new ClientHandler());
-                        }
-                    });
-            ChannelFuture future = client.connect(host,port).sync();
-            future.channel().writeAndFlush("Hello Netty Server ,I am a netty client");
-            future.channel().closeFuture().sync();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }finally {
-            group.shutdownGracefully();
-        }
-    }
-
-    public static class ClientHandler extends ChannelInboundHandlerAdapter {
-        public void channelActive(ChannelHandlerContext ctx) throws Exception {
-            System.out.println("NettyClient Active");
-        }
-
-        @Override
-        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-            System.out.println("NettyClient read Message:"+msg);
-        }
-    }
-
-}
-=======
-package com.github.sources.network;
-
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.*;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -104,6 +51,7 @@ public class NettyClient {
     }
 
     public static class ClientHandler extends ChannelInboundHandlerAdapter {
+        @Override
         public void channelActive(ChannelHandlerContext ctx) throws Exception {
             System.out.println("NettyClient Active");
         }
@@ -114,4 +62,3 @@ public class NettyClient {
         }
     }
 }
->>>>>>> 53a2c382af697e2464c9c2a9cf5506d6eb213d9c
--- source-aall/src/main/java/com/github/sources/structure/BinTreeNode.java
+++ source-aall/src/main/java/com/github/sources/structure/BinTreeNode.java
@@ -1,109 +1,3 @@
-<<<<<<< HEAD
-package com.github.sources.structure;
-
-/**
- *
- **/
-public class BinTreeNode {
-
-    private Node root;
-
-    public static void main(String[] args) {
-        int[] arr = {8, 5, 2, 1, 3, 4, 6, 7, 9};
-        BinTreeNode tree = new BinTreeNode();
-        for (int i = 0,len = arr.length; i < len; i++) {
-            tree.insert(arr[i]);
-        }
-        System.out.println("前序遍历：");
-        tree.preTraverse(tree.root);
-        System.out.println("");
-        System.out.println("中序遍历：");
-        tree.midTraverse(tree.root);
-        System.out.println("");
-        System.out.println("后序遍历：");
-        tree.postTraverse(tree.root);
-        System.out.println("");
-
-        System.out.println(hasPathSum(tree.root, 15));
-
-        invertNode(tree.root);
-        tree.preTraverse(tree.root);
-        System.out.println();
-    }
-
-    public static Node invertNode(Node node) {
-        if (node == null) {
-            return null;
-        }
-        Node temp = node.left;
-        node.left = invertNode(node.right);
-        node.right = invertNode(temp);
-        return node;
-    }
-
-    public static boolean hasPathSum(Node root, int sum) {
-        if(root == null){
-            return false;
-        }
-        if(root.left == null && root.right == null && sum - root.value == 0){
-            return true;
-        }
-        return hasPathSum(root.left, sum - root.value) || hasPathSum(root.right, sum - root.value);
-    }
-
-    //后序遍历
-    public void postTraverse(Node node) {
-        if (node != null) {
-            postTraverse(node.left);
-            postTraverse(node.right);
-            System.out.print(node.value + " ");
-        }
-    }
-
-    //中序遍历
-    public void midTraverse(Node node) {
-        if (node != null) {
-            midTraverse(node.left);
-            System.out.print(node.value + " ");
-            midTraverse(node.right);
-        }
-    }
-
-    //前序遍历
-    public void preTraverse(Node node) {
-        if (node != null) {
-            System.out.print(node.value + " ");
-            preTraverse(node.left);
-            preTraverse(node.right);
-        }
-    }
-
-    public void insert(int data) {
-        root = insert(root, data);
-    }
-
-    public Node insert(Node node, int value) {
-        if (node == null) {
-            node = new Node(value);
-        } else if (value > node.value) {
-            node.right = insert(node.right, value);
-        } else {
-            node.left = insert(node.left, value);
-        }
-        return node;
-    }
-
-    static class Node {
-        int value;
-        Node left;
-        Node right;
-
-        public Node(int value) {
-            this.value = value;
-        }
-    }
-}
-=======
 package com.github.sources.structure;
 
 /** */
@@ -119,13 +13,13 @@ public class BinTreeNode {
         }
         System.out.println("前序遍历：");
         tree.preTraverse(tree.root);
-        System.out.println();
+        System.out.println("");
         System.out.println("中序遍历：");
         tree.midTraverse(tree.root);
-        System.out.println();
+        System.out.println("");
         System.out.println("后序遍历：");
         tree.postTraverse(tree.root);
-        System.out.println();
+        System.out.println("");
 
         System.out.println(hasPathSum(tree.root, 15));
 
@@ -154,7 +48,7 @@ public class BinTreeNode {
         return hasPathSum(root.left, sum - root.value) || hasPathSum(root.right, sum - root.value);
     }
 
-    /** 后序遍历 */
+    // 后序遍历
     public void postTraverse(Node node) {
         if (node != null) {
             postTraverse(node.left);
@@ -163,11 +57,7 @@ public class BinTreeNode {
         }
     }
 
-    /**
-     * 中序遍历
-     *
-     * @param node
-     */
+    // 中序遍历
     public void midTraverse(Node node) {
         if (node != null) {
             midTraverse(node.left);
@@ -176,11 +66,7 @@ public class BinTreeNode {
         }
     }
 
-    /**
-     * 前序遍历
-     *
-     * @param node
-     */
+    // 前序遍历
     public void preTraverse(Node node) {
         if (node != null) {
             System.out.print(node.value + " ");
@@ -214,4 +100,3 @@ public class BinTreeNode {
         }
     }
 }
->>>>>>> 53a2c382af697e2464c9c2a9cf5506d6eb213d9c
