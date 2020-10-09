ebdc61354f9acd0c4831aff2c1bcf033d536a547
hairen.lhr <hairen.lhr@alibaba-inc.com>
2020-10-09 20:14:15 +0800

    merge

---
+++ source-dubbo/source-dubbo-consumer/src/main/java/com/github/sources/dubbo/consumer/controller/UserController2.java
@@ -0,0 +1,12 @@
+package com.github.sources.dubbo.consumer.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hairen.long
+ * @date 2020/10/9
+ */
+@RestController
+public class UserController2 {
+
+}
